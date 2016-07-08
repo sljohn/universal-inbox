7e912e0e9f1ba51ee828d16bed85791f290e4ae7
Nick Barry <niko.barry@gmail.com>
2016-07-08 15:37:53 -0700

    chore (deployment): .env configuration
    Update how .env and other environment vars are handled, to be consistent between local and deployment settings

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
+'use strict';
+
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
-const dotenv = require('dotenv');
 const expressValidator = require('express-validator');
 const db = require('./db/db');
 
 /*
 ** Load local enviroment variables from .env file where secrets and keys are configured.
 */
-dotenv.load({ path: '.env' });
+
+let dotenv;
+if (process.env.RUN_MODE === 'PRODUCTION') {
+  dotenv = process.env;
+} else {
+  dotenv = require('dotenv');
+  dotenv.load({ path: '.env' });
+}
 
 /*``
 ** Route Controllers
