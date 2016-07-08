728d427653966e8b12b2783d94514201eac2dbb1
Nick Barry <niko.barry@gmail.com>
2016-07-07 19:15:15 -0700

    style (server): Minor style updates to server files
     - Replace some line breaks
     - Add .env to .gitignore
     - Replace var with const

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./server/server.js');
 
-var port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port);
 console.log('Server now listening on port ' + port);
