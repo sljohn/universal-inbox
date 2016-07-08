8aa21c87b51b2ce1b5bcc31c3bbda8130c43068e
bterrot <torre.bonnie@gmail.com>
2016-07-08 09:46:22 -0700

    chore: Remove superfluous app folder
    I had used this folder to draft several quick tests, but it was no longer being used.

--- app/app.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('Main', [])
-  .controller('MainCtrl', function($scope){
-    $scope.messTitle = "Message Title Here";
-    $scope.messBody = "Message Body Here";
-  })
-  .factory('MainFactory', function(){
-
-  });
