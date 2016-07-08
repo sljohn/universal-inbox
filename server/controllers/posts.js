67aab15f3ad2d556121130f64878b557e1e59925
John Slape <slape.john@gmail.com>
2016-07-08 15:18:40 -0700

    feat web-server: getting tweets
    Added the retrieval of tweets to the getAllPosts function in posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,16 @@
 const dummyTweets = require('./dummyTweets.json');
+const twitter = require('./twitter.js');
+
+const getTweets = function() {
+  let tweets = [];
+  twitter.getTweets('makersquare').then(function(res) { //TODO: don't hardcode username
+    tweets = res;
+  });
+  return tweets;
+}
 
 exports.getAllPosts = (req, res) => {
-  res.send(dummyTweets);
+  //res.send(dummyTweets);
+  res.send(getTweets());
 };
+
