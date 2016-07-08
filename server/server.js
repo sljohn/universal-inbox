024f5e6e9b0974c63f49672628bf6761ab6fc308
Nick Barry <niko.barry@gmail.com>
2016-07-08 14:52:14 -0700

    chore: whitespace corrections per style guide

--- server/server.js
+++ server/server.js
@@ -33,8 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 app.use(session({
   resave: true,
-  // Do not initialize a session until a user is signed in.
-  saveUninitialized: false,
+  saveUninitialized: false, // Do not initialize a session until a user is signed in.
   secret: process.env.SESSION_SECRET || dotenv.SESSION_SECRET,
 }));
 app.use(express.static(path.join(__dirname, '../public')));
