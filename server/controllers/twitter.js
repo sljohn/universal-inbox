cc1df5435bbc18f60c198e474e6da80c9796bf89
Nick Barry <niko.barry@gmail.com>
2016-07-08 17:06:10 -0700

    fix (server): correct typo in 'twitter' module require statement
    The require statement asked for Twitter, and should have been asking for twitter.
    
    Also clean up per styleguide

--- server/controllers/twitter.js
+++ server/controllers/twitter.js
@@ -1,27 +1,24 @@
-const Twitter = require('Twitter');
-// const dotenv = require('dotenv');
+const Twitter = require('twitter');
 const path = require('path');
 
-// dotenv.load({path: path.__dirname + '/../.env'});
-
-console.log(process.env)
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRENT,
-  bearer_token: process.env.TWITTER_BEARER_TOKEN
+  bearer_token: process.env.TWITTER_BEARER_TOKEN,
 });
 
-function getTweets (username, since_id) {
+function getTweets(username, sinceId) {
   //prepend all usernames with %40, replacing the @ symbol if provided
   username = username.replace(/^(@|%40)?/, '%40');
 
   return new Promise(function (resolve, reject) {
-    const query = {q: username, since_id: since_id}
-    client.get('search/tweets', {q: username}, function(error, tweets, response) {
+    const query = { q: username, since_id: sinceId };
+    client.get('search/tweets', { q: username }, function (error, tweets, response) {
       if (error) {
         reject(error);
-      };
-    resolve(tweets);
+      }
+
+      resolve(tweets);
     });
   });
 };
