cf374f1b316e3702d641c054e8431d7caf83e6ae
Nick Barry <niko.barry@gmail.com>
2016-07-11 16:26:57 -0700

    style: Update files per airbnb files

--- client/app/services/tweet.factory.js
+++ client/app/services/tweet.factory.js
@@ -1,23 +1,25 @@
+'use strict';
+
+function TweetsFactory($http, logger) {
+  function getTweets() {
+    return $http({
+      method: 'GET',
+      url: 'api/tweets',
+    }).then(function successCallback(resp) {
+      return resp.data;
+    },
+  function errorCallback(resp) {
+      logger.logError(resp);
+    });
+  }
+
+  return {
+    getTweets,
+  };
+}
+
 angular
   .module('universal-inbox.TweetsFactory', [])
   .factory('TweetsFactory', TweetsFactory);
 
-  TweetsFactory.$inject = ['$http', 'logger'];
-  
-  function TweetsFactory($http, logger) {
-
-    function getTweets() {
-      return $http({
-        method: 'GET',
-        url: 'api/tweets'
-      }).then(function successCallback(resp) {
-        return resp.data;
-      }, function errorCallback(resp) {
-        logger.logError(resp);
-      });
-    }
-
-    return {
-      getTweets
-    };
-  }
+TweetsFactory.$inject = ['$http', 'logger'];
--- public/app/services/post.factory.js
+++ public/app/services/post.factory.js
@@ -1,16 +1,17 @@
+'use strict';
+
+function PostsFactory(/* $http */) {
+  function myCoolFnc() {
+    // function body
+  }
+
+  return { myCoolFnc };
+}
+
 angular
   .module('universal-inbox.PostsFactory', [])
   .factory('PostsFactory', PostsFactory);
 
-  // A different method to add dependency injection.
-  PostsFactory.$inject = ['$http'];
+// A different method to add dependency injection.
+PostsFactory.$inject = ['$http'];
 
-  function PostsFactory($http) {
-    function myCoolFnc() {
-      // function body
-    }
-
-    return {
-      myCoolFnc
-    };
-  }
