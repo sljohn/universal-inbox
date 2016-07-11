ecefb731d3b4eccae576d2aac0a8cba559f34abc
Justin Lien <lien.justin.m@gmail.com>
2016-07-11 11:01:11 -0700

    config: move development back into client folder
    - Setup front-end dev server to server index.html to avoid CORS error
    when opening index.html alone.
    - Copy and configure index.html to load correctly.

---
+++ client/app/services/post.factory.js
@@ -0,0 +1,13 @@
+angular
+  .module('universal-inbox.PostsFactory', [])
+  .factory('PostsFactory', function() {
+
+    function myCoolFnc() {
+      // function body
+      // return $http
+    }
+
+    return {
+      myCoolFnc: myCoolFnc
+    };
+  });
