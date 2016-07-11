cbb384879350f3153ba84ac744619ebe38a1c09e
Nick Barry <niko.barry@gmail.com>
2016-07-11 11:49:34 -0700

    style: Clean up posts.js per Airbnb style

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,14 +1,14 @@
 'use strict'; //node requires this for the use of 'let'
-const dummyTweets = require('../db/dummyTweets.json');
 const twitter = require('./twitter.js');
 
-const getTweets = function() {
+const getTweets = function () {
   let tweets = [];
-  twitter.getTweets('makersquare').then(function(res) { //TODO: don't hardcode username
+  twitter.getTweets('makersquare').then(function (res) { //TODO: don't hardcode username
     tweets = res;
-  });
+  }
+  );
   return tweets;
-}
+};
 
 exports.getAllPosts = (req, res) => {
   //res.send(dummyTweets);
