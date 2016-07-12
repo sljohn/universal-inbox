e65675d66084375a674ce992df8fada9122f2c79
Justin Lien <lien.justin.m@gmail.com>
2016-07-11 17:16:38 -0700

    feat: load tweet from dummy file

--- public/app/main.controller.js
+++ public/app/main.controller.js
@@ -15,12 +15,29 @@ function MainController(TweetsFactory) {
   vm.dateTime = new Date().toLocaleString();
 
   // Saving this method when getTweets is a real
-  // promoise
+  // promise.
   // TweetsFactory.getTweets().then((data) => {
   //   console.log('data: ', data);
   // });
 
+
   const tweets = TweetsFactory.getTweets();
 
   console.log(tweets);
+
+  vm.tweets = TweetsFactory.getTweets();
+
+  /*tweets.forEach(function(tweet) {
+    console.log('tweet.text: ', tweet.text);
+    console.log('tweet.user.name: ', tweet.user.name);
+    console.log('tweet.user.screen_name: @', tweet.user.screen_name);
+    console.log('tweet.user.profile_image_url: ', tweet.user.profile_image_url);
+    console.log('tweet.user.profile_image_url_https: ', tweet.user.profile_image_url_https);
+    console.log('tweet.created_at: ', tweet.created_at);
+    // console.log('tweet: ', tweet);
+    // console.log('tweet: ', tweet);
+
+
+  });*/
+
 }
