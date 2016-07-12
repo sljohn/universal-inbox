5b5ae66f0e5e4fe775a8ed45fcf4bd68db51e10b
Nick Barry <niko.barry@gmail.com>
2016-07-12 11:44:20 -0700

    feat: Filter posts by completion status and searchbar
    User can now filter the posts by completion status and also by typing in the searchbar.

--- public/app/main.controller.js
+++ public/app/main.controller.js
@@ -3,6 +3,7 @@
 function MainController(TweetsFactory) {
   const vm = this;
 
+  //<editor-fold desc="Get and display tweets">
   vm.tweets = [];
 
   // Saving this method when getTweets is a real
@@ -16,17 +17,29 @@ function MainController(TweetsFactory) {
   // vm.tweets = TweetsFactory.getTweets();
 
   /*tweets.forEach(function(tweet) {
-    console.log('tweet.text: ', tweet.text);
-    console.log('tweet.user.name: ', tweet.user.name);
-    console.log('tweet.user.screen_name: @', tweet.user.screen_name);
-    console.log('tweet.user.profile_image_url: ', tweet.user.profile_image_url);
-    console.log('tweet.user.profile_image_url_https: ', tweet.user.profile_image_url_https);
-    console.log('tweet.created_at: ', tweet.created_at);
-    // console.log('tweet: ', tweet);
-    // console.log('tweet: ', tweet);
+   console.log('tweet.text: ', tweet.text);
+   console.log('tweet.user.name: ', tweet.user.name);
+   console.log('tweet.user.screen_name: @', tweet.user.screen_name);
+   console.log('tweet.user.profile_image_url: ', tweet.user.profile_image_url);
+   console.log('tweet.user.profile_image_url_https: ', tweet.user.profile_image_url_https);
+   console.log('tweet.created_at: ', tweet.created_at);
+   // console.log('tweet: ', tweet);
+   // console.log('tweet: ', tweet);
 
-  });*/
+   });*/
+  //</editor-fold>
 
+  //<editor-fold desc="Filter by completed posts">
+  vm.viewCompleted = false;
+
+  vm.matchesCompletedFilter = function (post) {
+    return post.completedByUser ? vm.viewCompleted : !vm.viewCompleted;
+  };
+
+  //vm.applyCompletedFilter = function (filterStr) {
+  //
+  //}
+  //</editor-fold>
 }
 
 angular
